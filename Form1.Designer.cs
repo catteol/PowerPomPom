c15729352d287e21ae965414fb5cb81024b310ce
Rexent <rexent_gx@rexlab.dev>
2023-04-28 16:15:43 +0900

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PowerPomPom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.FPSBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LogTextbox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AppPathDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.FPSBar)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FPSBar
+            // 
+            this.FPSBar.LargeChange = 1;
+            this.FPSBar.Location = new System.Drawing.Point(18, 49);
+            this.FPSBar.Maximum = 3;
+            this.FPSBar.Name = "FPSBar";
+            this.FPSBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.FPSBar.Size = new System.Drawing.Size(45, 222);
+            this.FPSBar.TabIndex = 0;
+            this.FPSBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.FPSBar.Value = 3;
+            this.FPSBar.MouseCaptureChanged += new System.EventHandler(this.FPSBar_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "FPS";
+            // 
+            // LogTextbox
+            // 
+            this.LogTextbox.Location = new System.Drawing.Point(97, 49);
+            this.LogTextbox.Name = "LogTextbox";
+            this.LogTextbox.ReadOnly = true;
+            this.LogTextbox.Size = new System.Drawing.Size(354, 222);
+            this.LogTextbox.TabIndex = 6;
+            this.LogTextbox.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Log";
+            // 
+            // AppPathDialog
+            // 
+            this.AppPathDialog.FileName = "StarRail.exe";
+            this.AppPathDialog.Filter = "Exe files|*.exe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "120";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "90";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "60";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "30";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(479, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 288);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LogTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FPSBar);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PowerPomPom";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FPSBar)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar FPSBar;
+        private System.Windows.Forms.RichTextBox LogTextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog AppPathDialog;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+
+
+namespace PowerPomPom
+{
+    public partial class Form1 : Form
+    {
+        private readonly int[] fpsArray = new int[] { 30, 60, 90, 120 };
+
+        private string RegGraphicSettingsKeyName = null;
+        private GraphicSettings graphicSettings = new GraphicSettings();
+        private bool isFirstLog = true;
+        private int currentFps = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ver = Assembly.GetEntryAssembly().GetName().Version;
+            this.Text = $"{this.Text} v{ver.Major}.{ver.Minor}.{ver.Build}";
+
+            // Search Registry entry
+            RegistryKey rootKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Cognosphere\Star Rail");
+            if (rootKey == null)
+            {
+                while (rootKey == null)
+                {
+                    DialogResult result = ShowRegNotFoundDialog();
+                    if (result == DialogResult.Cancel)
+                    {
+                        Application.Exit();
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                RegGraphicSettingsKeyName = Array.Find(rootKey.GetValueNames(), delegate (string name) { return name.ToLower().StartsWith("graphicssettings_model"); });
+                if (RegGraphicSettingsKeyName == null)
+                {
+                    while (RegGraphicSettingsKeyName == null)
+                    {
+                        DialogResult result = ShowRegNotFoundDialog();
+                        if (result == DialogResult.Cancel)
+                        {
+                            Application.Exit();
+                            return;
+                        }
+
+                    }
+                }
+
+                var regValueBytes = (byte[])rootKey.GetValue(RegGraphicSettingsKeyName);
+                var regValueStr = System.Text.Encoding.ASCII.GetString(regValueBytes);
+                graphicSettings = JsonConvert.DeserializeObject<GraphicSettings>(regValueStr);
+
+            }
+            WriteLog("Loaded configuration values from registry.");
+
+            // Set FPS slider to current value
+            currentFps = FPSBar.Value = Array.IndexOf(fpsArray, graphicSettings.FPS);
+            WriteLog($"Current FPS is {graphicSettings.FPS}.");
+
+            WriteLog("Successfully launched.");
+        }
+
+        private void FPSBar_Scroll(object sender, EventArgs e)
+        {
+            if (currentFps != FPSBar.Value)
+            {
+                currentFps = FPSBar.Value;
+                graphicSettings.FPS = fpsArray[currentFps];
+
+                // Write to Registry
+                var settingsJson = JsonConvert.SerializeObject(graphicSettings);
+                byte[] settingsBytes = Encoding.ASCII.GetBytes(settingsJson + "\0");
+
+                RegistryKey rootKey = Registry.CurrentUser.OpenSubKey($@"SOFTWARE\Cognosphere\Star Rail", true);
+                rootKey.SetValue($"{RegGraphicSettingsKeyName}", settingsBytes, RegistryValueKind.Binary);
+
+                WriteLog($"Change FPS to {fpsArray[FPSBar.Value]}");
+            }
+        }
+
+        private void WriteLog(string log)
+        {
+            LogTextbox.SelectionStart = LogTextbox.Text.Length;
+            LogTextbox.SelectionLength = 0;
+            LogTextbox.SelectedText = isFirstLog ? $"{System.DateTime.Now}: {log}" : $"\n{System.DateTime.Now}: {log}";
+            isFirstLog = false;
+            LogTextbox.ScrollToCaret();
+        }
+
+        private DialogResult ShowRegNotFoundDialog()
+        {
+            return MessageBox.Show("Registry key is not found.\nLaunch the game once and try again.", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+        }
+
+        private class GraphicSettings
+        {
+            public int FPS { get; set; }
+            public bool EnableVSync { get; set; }
+            public float RenderScale { get; set; }
+            public int ResolutionQuality { get; set; }
+            public int ShadowQuality { get; set; }
+            public int LightQuality { get; set; }
+            public int CharacterQuality { get; set; }
+            public int EnvDetailQuality { get; set; }
+            public int ReflectionQuality { get; set; }
+            public int BloomQuality { get; set; }
+            public int AAMode { get; set; }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
